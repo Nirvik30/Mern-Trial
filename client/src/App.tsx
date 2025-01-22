167cccddbb447f859242b028526374a97fe57721
Nirvik30 <166740525+Nirvik30@users.noreply.github.com>
2025-01-22 05:51:12 +0000

    three

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-white">
 
-    <h1>hey i am nirvik</h1>
+    <h1 className="bg-white text-black drop-shadow-2xl	 ">hey i am nirvik</h1>
+
     </div>
   )
 }
