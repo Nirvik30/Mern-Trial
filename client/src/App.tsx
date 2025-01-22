f4f05cd239adfa7968cbda2c742e2813b0a6a54f
Nirvik30 <166740525+Nirvik30@users.noreply.github.com>
2025-01-22 09:53:34 +0000

    four

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
+
+import { CalendarDemo } from "./trial/calender"
+import { CheckboxDemo } from "./trial/checkbox"
   
 
 const App = () => {
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-white">
 
     <h1 className="bg-white text-black drop-shadow-2xl	 ">hey i am nirvik</h1>
+<CalendarDemo />
+<CheckboxDemo/>
 
     </div>
   )
